45ca3c0f49ecc93f5144c2c3d18e688c7f4694b3
Josh <kelloggs989@live.com>
2022-08-12 15:17:17 -0400

    V0.6 (Boss) Update
    added boss that can be defeted over time. added methon of raising attack

--- GameFiles/gui.py
+++ GameFiles/gui.py
@@ -5,17 +5,17 @@ import os
 from pygame import mixer
 
 # Configs
-coins = 100
+coins = 0
 Health = 10
 Houses = 0
 workers = 1
 resets = 0
 Attack = 2
-Boss_Attack = 7
+Boss_Attack = 50
 mega_resets = 0
 Kills = 0
 EDam = 1
-
+Boss_Health = 100
 # this part of the config set the prices for the buy menu and the names for the item
 worker_cost = 100
 House_Cost = 10000
@@ -23,49 +23,83 @@ reset_cost = 1000000
 Health_Cost = 50
 mega_reset_cost = 4
 
-# This is the Gui Layouts
+# key list
+# window['-TEXT-'].update(coins)
+# window['-TEXT1-'].update(Attack)
+# window['-TEXT2-'].update(Health)
+# window['-TEXT3-'].update(Kills)
+# window['-TEXT4-'].update(workers)
+# window['-TEXT5-'].update(worker_cost)
+# window['-TEXT6-'].update(EDam)
+# window['-TEXT7-'].update(Boss_Health)
+# window['-TEXT8-'].update(Boss_Attack)
 
+# This is the Gui Layouts
 sg.theme('Dark Green 7')
 
-layout = [[sg.Text("Coins"), sg.Text(coins, key='-TEXT-'), sg.Button("Fight"), sg.Push(), sg.Text("kills"),
+layout = [[sg.Text("Coins"), sg.Text(coins, key='-TEXT-'), sg.Button("Fight"), sg.Button('Boss'),
+           [sg.Checkbox('Auto Work:', key='checkbox')],
+           sg.Push(), sg.Text("kills"),
            sg.Text(Kills, key='-TEXT3-')],
           [sg.Text("Health"), sg.Text(Health, key='-TEXT2-'), sg.Button("Buy Slave"), sg.Push(), sg.Text("Slave Cost"),
            sg.Text(worker_cost, key='-TEXT5-')],
-          [sg.Text("Attack"), sg.Text(Attack), sg.Button("Work"), sg.Push(), sg.Text("Difficulty"), sg.Text(EDam, key='-TEXT6-')],
-          [sg.Text("Workers"), sg.Text(workers, key='-TEXT4-'), sg.Button("Heal")],
-          [sg.Button("Quit"), sg.Button("Save")],
+          [sg.Text("Attack"), sg.Text(Attack, key='-TEXT1-'), sg.Button("Work"), sg.Button("Grind"), sg.Push(), sg.Text("Difficulty"),
+           sg.Text(EDam, key='-TEXT6-')],
+          [sg.Text("Workers"), sg.Text(workers, key='-TEXT4-'), sg.Button("Heal"), sg.Push(),
+           sg.Text("Boss Health"), sg.Text(Boss_Health, key='-TEXT7-')],
+          [sg.Button("Quit"), sg.Button("Save"), sg.Push(), sg.Text("Boss Attack"), sg.Text(Boss_Attack, key='-TEXT8-')],
           [sg.VPush(), sg.Text("https://github.com/ModDayHippie/IdleGame/tree/main for more updates"),
            [sg.Text("Goal of the game is to kill 1000 bad guys!!")],
                     [sg.Text(" The game will end if you have 0 health or negitive money")]]]
 
-                                  # these numpers are the window sixe first is width second is hight
-window = sg.Window('MDH-IdleGame', layout,size=(400, 280))
+                                # these numpers are the window sixe first is width second is hight
+window = sg.Window('MDH-ClickerGame', layout, size=(400, 300))
 
 # Main Event Loop
 
 while True:
     event, values = window.read()
     window['-TEXT-'].update(coins)
+    window['-TEXT1-'].update(Attack)
     window['-TEXT2-'].update(Health)
     window['-TEXT3-'].update(Kills)
     window['-TEXT4-'].update(workers)
+    window['-TEXT5-'].update(worker_cost)
+    window['-TEXT6-'].update(EDam)
+    window['-TEXT7-'].update(Boss_Health)
+    window['-TEXT8-'].update(Boss_Attack)
 
     if event in ('Quit'):
         break
 
+    elif values['checkbox'] == True:
+        print("hello")
+
     if event in ('Buy Slave'):
-        mixer.init()
-        mixer.music.load("coin.mp3")
-        mixer.music.set_volume(10)
-        mixer.music.play()
-        coins -= 1 * worker_cost
-        workers += 1
-        worker_cost += 200 * workers
-        # these lines update the window, test 1 - 3 are diffrent variables
-        window['-TEXT-'].update(coins)
-        window['-TEXT2-'].update(Health)
-        window['-TEXT4-'].update(workers)
-        window['-TEXT5-'].update(worker_cost)
+        if coins <= worker_cost:
+            window['-TEXT-'].update(coins)
+            window['-TEXT1-'].update(Attack)
+            window['-TEXT2-'].update(Health)
+            window['-TEXT3-'].update(Kills)
+            window['-TEXT4-'].update(workers)
+            window['-TEXT5-'].update(worker_cost)
+            window['-TEXT6-'].update(EDam)
+            window['-TEXT7-'].update(Boss_Health)
+            window['-TEXT8-'].update(Boss_Attack)
+        else:
+            mixer.init()
+            mixer.music.load("coin.mp3")
+            mixer.music.set_volume(10)
+            mixer.music.play()
+            coins -= 1 * worker_cost
+            workers += 1
+            worker_cost += 200 * workers
+            # these lines update the window, test 1 - 3 are diffrent variables
+            window['-TEXT-'].update(coins)
+            window['-TEXT2-'].update(Health)
+            window['-TEXT4-'].update(workers)
+            window['-TEXT5-'].update(worker_cost)
+            print("buy")
 
     if event in ('Save'):
         # this will save to an exel file
@@ -131,7 +165,6 @@ while True:
         window['-TEXT-'].update(coins)
     # if your coins are less then 0 you lose
     if coins < 0:
-
         os.system("EndMenu.py")
         os.close()
         # break
@@ -140,4 +173,92 @@ while True:
         os.system("EndMenu.py")
         os.close()
         # break
+
+    if event in ('Boss'):
+        # rng picks a number
+        Boss_Rng = random.randint(0, 10)
+        # if number is less then 8
+        if Boss_Rng < 9:
+            mixer.init()
+            mixer.music.load("oof.mp3")
+            mixer.music.set_volume(0.5)
+            mixer.music.play()
+            Health -= Boss_Attack
+            window['-TEXT2-'].update(Health)
+        else:
+            Boss_Health -= Attack
+            Boss_Attack += 3
+            mixer.init()
+            mixer.music.load("Sword.mp3")
+            mixer.music.set_volume(0.3)
+            mixer.music.play()
+            window['-TEXT-'].update(coins)
+            window['-TEXT2-'].update(Health)
+            window['-TEXT3-'].update(Kills)
+            window['-TEXT7-'].update(Boss_Health)
+            window['-TEXT8-'].update(Boss_Attack)
+
+    if event in ('Grind'):
+        Grind_Rng = random.randint(0, 10)
+        if Grind_Rng < 8:
+            coins += 8 * workers * 10
+            workers -= 1
+        else:
+            coins += 8 * workers * 10
+            window['-TEXT-'].update(coins)
+            window['-TEXT2-'].update(Health)
+            window['-TEXT3-'].update(Kills)
+            window['-TEXT4-'].update(workers)
+            window['-TEXT5-'].update(worker_cost)
+            window['-TEXT6-'].update(EDam)
+            window['-TEXT7-'].update(Boss_Health)
+            window['-TEXT8-'].update(Boss_Attack)
+
+    if Kills == 5:
+        Attack += 2
+        window['-TEXT-'].update(coins)
+        window['-TEXT1-'].update(Attack)
+        window['-TEXT2-'].update(Health)
+        window['-TEXT3-'].update(Kills)
+        window['-TEXT4-'].update(workers)
+        window['-TEXT5-'].update(worker_cost)
+        window['-TEXT6-'].update(EDam)
+        window['-TEXT7-'].update(Boss_Health)
+        window['-TEXT8-'].update(Boss_Attack)
+    if Kills == 10:
+        Attack += 3
+        window['-TEXT-'].update(coins)
+        window['-TEXT1-'].update(Attack)
+        window['-TEXT2-'].update(Health)
+        window['-TEXT3-'].update(Kills)
+        window['-TEXT4-'].update(workers)
+        window['-TEXT5-'].update(worker_cost)
+        window['-TEXT6-'].update(EDam)
+        window['-TEXT7-'].update(Boss_Health)
+        window['-TEXT8-'].update(Boss_Attack)
+    if Kills == 15:
+        Attack += 4
+        window['-TEXT-'].update(coins)
+        window['-TEXT1-'].update(Attack)
+        window['-TEXT2-'].update(Health)
+        window['-TEXT3-'].update(Kills)
+        window['-TEXT4-'].update(workers)
+        window['-TEXT5-'].update(worker_cost)
+        window['-TEXT6-'].update(EDam)
+        window['-TEXT7-'].update(Boss_Health)
+        window['-TEXT8-'].update(Boss_Attack)
+    if Kills == 20:
+        Attack += 5
+        window['-TEXT-'].update(coins)
+        window['-TEXT1-'].update(Attack)
+        window['-TEXT2-'].update(Health)
+        window['-TEXT3-'].update(Kills)
+        window['-TEXT4-'].update(workers)
+        window['-TEXT5-'].update(worker_cost)
+        window['-TEXT6-'].update(EDam)
+        window['-TEXT7-'].update(Boss_Health)
+        window['-TEXT8-'].update(Boss_Attack)
+
+
+
 window.close()
